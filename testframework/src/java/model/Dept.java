945848e4ed5c26db1a27f5b9a1c86aaef7f40f05
fifalianajennyh <124344145+fifalianajennyh@users.noreply.github.com>
2023-06-16 02:51:33 -0500

    sprint6 ok

--- testframework/src/java/model/Dept.java
+++ testframework/src/java/model/Dept.java
@@ -13,7 +13,11 @@ public class Dept{
         String s="test";
         String vString=s+s1;
        ModelView m=new ModelView(vString);
+       m.add("test1", "Jesus is my love and my life");
+       m.add("test2", "Jesus love you");
+       //m.add("test2", new String[] { "Tableau Mety"});
         return m;
     }
-    
 }
+//getParameter().getName
+//request.getParameter()
