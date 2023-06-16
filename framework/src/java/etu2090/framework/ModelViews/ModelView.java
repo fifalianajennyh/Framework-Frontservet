e7a73db0be6e3b8be851f40e174b65a94493b7af
fifalianajennyh <124344145+fifalianajennyh@users.noreply.github.com>
2023-06-16 02:46:16 -0500

    sprint5 ok

---
+++ framework/src/java/etu2090/framework/ModelViews/ModelView.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu2090.framework.ModelViews;
+
+/**
+ *
+ * @author itu
+ */
+public class ModelView {
+ private String view;
+
+    // constructor
+    public ModelView(String view) {
+        this.setView(view);
+    }
+
+    // setter
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    // getter
+    public String getView() {
+        return this.view;
+    }
+       
+}
+
---
+++ testframework/src/java/views/test.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : index
+    Created on : 28 mars 2023, 02:08:00
+    Author     : itu
+--%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/insertion.css">
+    <title>Bienvenue</title>
+</head>
+<body>
+    <h1>FRAMEWORK JENNY 2090</h1>
+    <p>Mety le izy</p>
+</body>
+</html>
